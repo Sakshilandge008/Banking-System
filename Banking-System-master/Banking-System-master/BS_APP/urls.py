b32b841902a632b9e8ad35ce04ca4cc6ff03a780
Sakshilandge008 <landgesakshi008@gmail.com>
2026-04-24 20:35:19 +0530

    commit

--- Banking-System-master/Banking-System-master/BS_APP/urls.py
+++ Banking-System-master/Banking-System-master/BS_APP/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import *
+from .views import home, user, transfer, add_transfer, history, add_user
 
 urlpatterns = [
-    path('', home),
-    path('users/',user),
-    path('transfer/<int:pk>',transfer),
-    path('add_transfer/<int:pk>',add_transfer),
-    path('history/',history)
+    path('', home, name='home'),
+    path('users/', user, name='users'),
+    path('transfer/<int:pk>', transfer, name='transfer'),
+    path('add_transfer/<int:pk>', add_transfer, name='add_transfer'),
+    path('history/', history, name='history'),
+
+    # ✅ ADD USER PAGE
+    path('add-user/', add_user, name='add_user'),
 ]
